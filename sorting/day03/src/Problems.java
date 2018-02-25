6714aa55f4ab62b207d91536dc96ebcd04ffb8b7
Nathan Yee <nathaniel.yee@students.olin.edu>
2018-02-25 14:10:39 -0500

    finished day03 sorting

--- sorting/day03/src/Problems.java
+++ sorting/day03/src/Problems.java
@@ -1,7 +1,16 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
-        // TODO
+        for (int i = 0; i < A.length; i++) {
+            A[i] += 100;
+        }
+        CountingSort.countingSort(A);
+        for (int i = 0; i < A.length; i++) {
+            A[i] -= 100;
+        }
     }
 
     /**
@@ -19,14 +28,37 @@ public class Problems {
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        // create 26 Linked Lists for each lowercase character
+        int b = 26;
+        LinkedList<String>[] L = new LinkedList[b];
+        for (int i = 0; i < b; i++) {
+            L[i] = new LinkedList<>();
+        }
+
+        // place each string into corresponding Linked List based on Nth character
+        for (String s : A) {
+            L[getNthCharacter(s, n)].add(s);
+        }
+
+        // put all numbers in the Linked Lists into A
+        int j = 0; // index in A to place numbers
+        for (LinkedList<String> list : L) {
+            Iterator it = list.iterator();
+            while (it.hasNext()) {
+                String s = (String) it.next();
+                A[j] = s;
+                j++;
+            }
+        }
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i = 0; i < stringLength; i++) {
+            countingSortByCharacter(S, i);
+        }
     }
 
     /**
