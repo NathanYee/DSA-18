7771cd8d7225ffd74f9e0dea92a9bed420fb32fb
Nathan Yee <nathaniel.yee@students.olin.edu>
2018-03-15 22:34:16 -0400

    did peakfinder

--- trees/day03/src/divide_and_conquer/PeakFinding.java
+++ trees/day03/src/divide_and_conquer/PeakFinding.java
@@ -1,5 +1,7 @@
 package divide_and_conquer;
 
+import java.util.Arrays;
+
 public class PeakFinding {
 
     // Return -1 if left is higher, 1 if right is higher, 0 if peak
@@ -9,6 +11,30 @@ public class PeakFinding {
         if (i < nums.length - 1 && nums[i] < nums[i + 1])
             return 1;
         return 0;
+    }
+
+
+    public static int findOneDPeak(int[] nums) {
+        // base cases
+        if (nums.length == 1) {
+            return 0;
+        } else if (nums.length == 2) {
+            if (nums[0] > nums[1]) {
+                return 0;
+            }
+            return 1;
+        }
+
+        // divide
+        int i = nums.length / 2;
+        if (nums[i] < nums[i - 1]) {
+            return findOneDPeak(Arrays.copyOfRange(nums, 0, i - 1));
+        } else if (nums[i] < nums[i + 1]) {
+            return i + 1 + findOneDPeak(Arrays.copyOfRange(nums, i + 1, nums.length));
+        }
+
+        // get lucky
+        return i;
     }
 
     // Return -1 if left is higher, 1 if right is higher, 0 if peak
@@ -49,15 +75,50 @@ public class PeakFinding {
         return maxIndex;
     }
 
+    // divide rows
+    public static int[] findTwoDPeakY(int[][] nums, int x0, int x1, int y0, int y1) {
+        int y = (y1 - y0) / 2 + y0;
+        int x = maxXIndex(y, x0, x1, nums);
 
-    public static int findOneDPeak(int[] nums) {
-        // TODO
-        return 0;
+        if (y == 0) {
+            return new int[]{y, x};
+        }
+
+        int peak = peakY(x, y, nums);
+        switch (peak) {
+            case -1:
+                return findTwoDPeakX(nums, x0, x1, y0, y);
+            case 1:
+                return findTwoDPeakX(nums, x0, x1, y + 1, y1);
+            case 0:
+                return new int[]{y, x};
+        }
+        return new int[]{y, x};
+    }
+
+    // divide columns
+    public static int[] findTwoDPeakX(int[][] nums, int x0, int x1, int y0, int y1) {
+        int x = (x1 - x0) / 2 + x0;
+        int y = maxYIndex(x, y0, y1, nums);
+
+        if (x == 0) {
+            return new int[]{y, x};
+        }
+
+        int peak = peakX(x, y, nums);
+        switch (peak) {
+            case -1:
+                return findTwoDPeakY(nums, x0, x, y0, y1);
+            case 1:
+                return findTwoDPeakY(nums, x+1, x1, y0, y1);
+            case 0:
+                return new int[]{y, x};
+        }
+        return new int[]{y, x};
     }
 
     public static int[] findTwoDPeak(int[][] nums) {
-        // TODO
-        return null;
+        return findTwoDPeakX(nums, 0, nums[0].length, 0, nums.length);
     }
 
 }
