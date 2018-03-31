544d2696c86297a50a18f4790db9be55008e1c02
Nathan Yee <nathaniel.yee@students.olin.edu>
2018-03-31 13:44:42 -0400

    re-did permutations

--- graphs/day01/src/Permutations.java
+++ graphs/day01/src/Permutations.java
@@ -5,6 +5,20 @@ import java.util.Set;
 
 public class Permutations {
 
+    private static void backtrack(LinkedList<Integer> cur, Set<Integer> unused, List<List<Integer>> perms) {
+        if (unused.isEmpty()) {
+            perms.add(new LinkedList<>(cur));
+        }
+
+        for (int u : new LinkedList<>(unused)) {
+            cur.addLast(u);
+            unused.remove(u);
+            backtrack(cur, unused, perms);
+            unused.add(u);
+            cur.removeLast();
+        }
+    }
+
     public static List<List<Integer>> permutations(List<Integer> A) {
         LinkedList<Integer> curr = new LinkedList<>();
         Set<Integer> unused = new HashSet<>(A);
