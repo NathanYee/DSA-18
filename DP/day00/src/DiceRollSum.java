3b7bff5e9fcec31e98732a4858bedaa6122c755f
Nathan Yee <nathaniel.yee@students.olin.edu>
2018-04-23 21:03:22 -0400

    did dice roll sum

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,28 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(N)
+    // Space: O(N)
     public static int diceRollSum(int N) {
-        // TODO
-        return 0;
+
+        HashMap<Integer, Integer> memo = new HashMap<>();
+
+        return diceRollRec(N, memo);
+    }
+
+    private static int diceRollRec(int N, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(N)) return memo.get(N);
+        if (N < 0) return 0;
+        if (N == 0) return 1;
+
+        int accumulator = 0;
+        for (int i = 1; i <= 6; i++) {
+            int subroll = diceRollRec(N - i, memo);
+            memo.put(N - i, subroll);
+            accumulator += subroll;
+        }
+        return accumulator;
     }
 
 }
